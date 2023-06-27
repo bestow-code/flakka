180c601a4b5a93d8f27214bab4675d1ec6434575
Ryan Hanks <ryan.hanks@bestow.world>
2023-06-27 12:00:59 -0500

    test(core_entry): test EntryRefFactory

---
+++ packages/core_entry/test/src/entry_ref/entry_ref_factory_test.dart
@@ -0,0 +1,10 @@
+import 'package:core_entry/core_entry.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('increment', () {
+    final factory = EntryRefFactory.increment();
+    expect(factory.create(),
+        isA<EntryRef>().having((p0) => p0.value, 'ref value', '1'));
+  });
+}
