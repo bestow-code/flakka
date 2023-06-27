330567d3794870148f0f0906d8166413e385c755
Ryan Hanks <ryan.hanks@bestow.world>
2023-06-27 15:07:47 -0500

    fix: specify relative directory for test

--- packages/core_entry_store/core_entry_store/test/verify_build_test.dart
+++ packages/core_entry_store/core_entry_store/test/verify_build_test.dart
@@ -2,5 +2,8 @@ import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ensure_build', expectBuildClean);
+  test('ensure_build', () {
+    expectBuildClean(
+        packageRelativeDirectory: 'packages/core_entry_store/core_entry_store');
+  });
 }
