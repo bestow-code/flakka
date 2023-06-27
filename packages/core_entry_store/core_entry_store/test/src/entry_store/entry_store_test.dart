b335b1cd5fc092e331dc4ddcea1a7016c464c2cc
Ryan Hanks <ryan.hanks@bestow.world>
2023-06-27 16:34:27 -0500

    fix: test

--- packages/core_entry_store/core_entry_store/test/src/entry_store/entry_store_test.dart
+++ packages/core_entry_store/core_entry_store/test/src/entry_store/entry_store_test.dart
@@ -1,5 +1,5 @@
 // import 'package:core_entry_store/core_entry_store.dart';
-import 'package:mocktail/mocktail.dart';
+// import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 // class MockEntryStoreDelegate extends Mock
