db7e8804914acd9605afc694ad4d3e4c08204d32
ryanhanks-bestow <87093311+ryanhanks-bestow@users.noreply.github.com>
2023-06-28 13:16:42 -0500

    chore: introduce add_core_entry_store_firestore (#15)

---
+++ packages/core_entry_store/core_entry_store_firestore/lib/core_entry_store_firestore.dart
@@ -0,0 +1,10 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+/// Firestore adapter of Core Entry Store
+library core_entry_store_firestore;
+
+export 'src/entry_ref_log/entry_ref_log.dart';
+export 'src/entry_ref_log/entry_ref_log_factory.dart';
+export 'src/entry_store_firestore/entry_store_firestore.dart';
+export 'src/entry_store_firestore/head_entry_ref.dart';
+export 'src/entry_store_firestore/head_entry_ref_factory.dart';
