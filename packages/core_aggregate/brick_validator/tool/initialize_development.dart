52a17ae81b447bc30f57cedce2c39b017f1791ff
Ryan Hanks <ryan.hanks@bestow.world>
2023-06-29 15:59:08 -0500

    chore: fixup e2e development initialization

--- packages/core_aggregate/brick_validator/tool/initialize_development.dart
+++ packages/core_aggregate/brick_validator/tool/initialize_development.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 import 'dart:io';
 
-import 'package:core_aggregate_e2e_tests/generate.dart';
+import 'package:brick_validator/generate.dart';
 import 'package:path/path.dart' as p;
 
 void main() async {
