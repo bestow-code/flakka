520046ea9ef773bec6113f874688aa6c1c87bfe1
Ryan Hanks <ryan.hanks@bestow.world>
2023-07-25 20:43:46 -0500

    wip

--- packages/core_datastore/core_datastore_sembast/lib/src/datastore_remote_sembast.dart
+++ packages/core_datastore/core_datastore_sembast/lib/src/datastore_remote_sembast.dart
@@ -1,6 +1,7 @@
 import 'package:collection/collection.dart';
 import 'package:core_data/core_data.dart';
 import 'package:core_datastore/core_datastore.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:sembast/sembast.dart';
 
 class DatastoreRemoteSembast<Event extends CoreEvent, State extends CoreState,
@@ -16,7 +17,7 @@ class DatastoreRemoteSembast<Event extends CoreEvent, State extends CoreState,
 
   final headRefStore = StoreRef<String, Map<String, dynamic>>('head');
   final entryStore = StoreRef<String, Map<String, dynamic>>('entry');
-  final eventsStore = StoreRef<String, List<Map<String, dynamic>>>('events');
+  final eventsStore = StoreRef<String, Map<String, dynamic>>('events');
 
   @override
   Future<({Ref? instance, Ref main})> initialize({
@@ -48,38 +49,65 @@ class DatastoreRemoteSembast<Event extends CoreEvent, State extends CoreState,
   @override
   Future<void> appendEvents(
     Iterable<Event> events, {
-    required Entry entry,
-  }) async {
-    await _database.transaction((transaction) async {
-      events.map(_dataConverter.eventConverter.toJson);
-      await entryStore.record(entry.ref.value).put(transaction, entry.toJson());
-      await headRefStore
-          .record('instance')
-          .put(transaction, entry.ref.toJson());
-    });
-  }
+    required Ref ref,
+    required Ref parent,
+    required DateTime createdAt,
+    required int sequenceNumber,
+  }) =>
+      _database.transaction((transaction) async {
+        await eventsStore.record(ref.value).add(
+              _database,
+              Events(ref: ref, data: events).toJson(
+                _dataConverter.eventConverter.toJson,
+              ),
+            );
+        await entryStore.record(ref.value).put(
+              transaction,
+              Entry(ref: ref, refs: [parent], createdAt: createdAt).toJson(),
+            );
+        await headRefStore.record('instance').put(transaction, ref.toJson());
+      });
 
   @override
-  Future<void> appendMerge({required Entry entry}) {
+  Future<void> appendMerge(
+    Ref merge, {
+    required Ref ref,
+    required Ref parent,
+    required DateTime createdAt,
+    required int sequenceNumber,
+  }) =>
+      _database.transaction((transaction) async {
+        await entryStore.record(ref.value).put(
+              transaction,
+              Entry(ref: ref, refs: [parent, merge], createdAt: createdAt)
+                  .toJson(),
+            );
+        await headRefStore.record('instance').put(transaction, ref.toJson());
+      });
+
+  @override
+  Future<void> forward(
+    Ref ref, {
+    required DateTime createdAt,
+    required int sequenceNumber,
+  }) {
+    // TODO: implement forward
     throw UnimplementedError();
   }
 
   @override
-  Future<void> forward(Ref ref) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<bool> publish(Ref ref, Iterable<Ref> from) async =>
+  Future<void> publish(
+    Ref ref, {
+    required Iterable<Ref> from,
+    required DateTime createdAt,
+    required int sequenceNumber,
+  }) async =>
       _database.transaction((transaction) async {
         final json = await headRefStore.record('main').get(transaction);
         if (json != null) {
           final current = Ref.fromJson(json);
           if (from.contains(current)) {
             await headRefStore.record('main').put(transaction, ref.toJson());
-            return true;
-          } else {
-            return false;
           }
         } else {
           throw UnsupportedError('main ref not initialized');
@@ -87,27 +115,53 @@ class DatastoreRemoteSembast<Event extends CoreEvent, State extends CoreState,
       });
 
   @override
-  Stream<CollectionSnapshot<Entry>> get entryCollectionSnapshot {
-    return entryStore
-        .query()
-        .onSnapshots(_database)
-        .map((snapshots) => snapshots.map((e) => e.value).whereNotNull().map(Entry.fromJson))
-        .map(
-          (entries) => (
-            hasPendingWrites: true,
-            snapshots:
-                entries.map((entry) => (hasPendingWrite: true, snapshot: entry))
-          ),
-        );
-  }
-
-  @override
-  Stream<CollectionSnapshot<Iterable<Event>>> get eventsCollectionSnapshot =>
-      throw UnimplementedError();
+  Stream<CollectionSnapshot<Entry>> get entryCollectionSnapshot => entryStore
+          .query()
+          .onSnapshots(_database)
+          .map(
+            (snapshots) => snapshots
+                .map((e) => e.value)
+                .whereNotNull()
+                .map(Entry.fromJson),
+          )
+          .map(
+            (entries) => (
+              hasPendingWrites: true,
+              snapshots:
+                  entries.map((entry) => (hasPendingWrite: true, data: entry))
+            ),
+          )
+          .startWith(
+        (hasPendingWrites: false, snapshots: <DataSnapshot<Entry>>[]),
+      );
 
   @override
   Stream<Ref> get mainRef => headRefStore
       .record('main')
       .onSnapshot(_database)
-      .map((event) => Ref.fromJson(event!.value));
+      .map((event) => event?.value)
+      .whereNotNull()
+      .map(Ref.fromJson);
+
+  @override
+  Stream<CollectionSnapshot<Events<Event>>> get eventsCollectionSnapshot =>
+      entryStore
+          .query()
+          .onSnapshots(_database)
+          .map(
+            (snapshots) => snapshots.map((e) => e.value).whereNotNull().map(
+                  (events) => Events.fromJson(
+                    events,
+                    (event) => _dataConverter.eventConverter
+                        .fromJson(event! as Map<String, dynamic>),
+                  ),
+                ),
+          )
+          .map(
+            (entries) => (
+              hasPendingWrites: true,
+              snapshots:
+                  entries.map((entry) => (hasPendingWrite: true, data: entry))
+            ),
+          );
 }
