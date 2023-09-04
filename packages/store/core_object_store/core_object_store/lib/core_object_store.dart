0538449edbf414ea6ed4ad2c3a8b389c3f8ef154
Ryan Hanks <ryan.hanks@bestow.world>
2023-09-04 00:16:13 -0500

    wip

--- packages/store/core_object_store/core_object_store/lib/core_object_store.dart
+++ packages/store/core_object_store/core_object_store/lib/core_object_store.dart
@@ -5,8 +5,8 @@ library core_object_store;
 
 export 'src/core_object_store.dart';
 export 'src/core_object_store_factory.dart';
-export 'src/core_object_store_provider.dart';
+export 'src/core_object_store_factory_provider.dart';
 export 'src/entry_props.dart';
 export 'src/object_store/object_store.dart';
 export 'src/object_store/object_store_factory.dart';
-export 'src/object_store/object_store_provider.dart';
+export 'src/object_store/object_store_factory_provider.dart';
--- packages/store/core_object_store/core_object_store_remote/lib/core_object_store_remote.dart
+++ packages/store/core_object_store/core_object_store_remote/lib/core_object_store_remote.dart
@@ -5,8 +5,8 @@ library core_object_store_remote;
 
 export 'src/core_object_store_remote.dart';
 export 'src/core_object_store_remote_factory.dart';
-export 'src/core_object_store_remote_provider.dart';
+export 'src/core_object_store_remote_factory_provider.dart';
 export 'src/object_store_remote/object_store_remote.dart';
 export 'src/object_store_remote/object_store_remote_factory.dart';
-export 'src/object_store_remote/object_store_remote_provider.dart';
+export 'src/object_store_remote/object_store_remote_factory_provider.dart';
 export 'src/object_store_remote/object_store_remote_state.dart';
